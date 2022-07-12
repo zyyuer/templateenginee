c44020035363c6ac62e156d3176a8e22bfa15c88
Administrator <devoteamwork@163.com>
2022-07-12 10:28:59 +0800

    删除了favicon: false 增加了yml配置contextpath给jsp使用 解决了swagger显示异常的问题clean

--- src/main/java/cn/tangue/templateenginee/exception/ResponseResult.java
+++ src/main/java/cn/tangue/templateenginee/exception/ResponseResult.java
@@ -1,7 +1,7 @@
 package cn.tangue.templateenginee.exception;
 
 import java.lang.annotation.*;
-
+//可以不要了
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Documented
